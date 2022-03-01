5be6b0cf5e658c7ddb722ee30e9168165b61749d
CFLChrisz <100291263+CFLChrisz@users.noreply.github.com>
2022-03-01 10:28:03 -0500

    Exercises

--- exercise1.py
+++ exercise1.py
@@ -1,13 +1,9 @@
 #Créer une fonction prenant en paramètre 
-#l'année de naissance d'une personne et 
-#lui retournant son l'âge qu'elle aura
+#l'année de naissance d'une personne 
+#et lui retournant son l'âge qu'elle aura 
 #à sa fête en 2022.
 
-def add_carre(n1=1, n2=1):
- return n1**2 + n2**2
-
-n1 = 10
-n2 = 20
-print(add_carre(n1, n2))
-print(add_carre(n1))
-print(add_carre())
+def age_en_2022(annee_naissance):
+  return 2022-annee_naissance
+age = 1993
+print(age_en_2022(age))
