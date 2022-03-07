2ada68b661bf95c00cfd943b3b67de648b1f011d
CFLChrisz <100291263+CFLChrisz@users.noreply.github.com>
2022-03-07 11:20:22 -0500

    lundi 7 mars

---
+++ animal.py
@@ -0,0 +1,27 @@
+jour_fete = int(input("entrex votre jour de fète en chiffre! ")) 
+mois_fete = int(input("entrer vore mois en chiffre! "))
+
+if jour_fete==7 and mois_fete==3:
+    print("ah ben tabarnack bnne ")
+else:
+    print("mission failed bette luck next time!")
+
+def saisons(été,automne,hiver,printemps):
+    return()
+
+été = jour_fete== 1 31 and mois_fete== [6 , 8]
+automne = jour_fete== 1 < 31 and mois_fete== 9 < 11
+hiver= jour_fete== 1 < 31 and mois_fete== 12 < 2
+printemps = jour_fete== 1 < 31 and mois_fete== 3 < 5
+
+if été:
+    print("t née en été twé! Ayoye ")
+
+if printemps:
+    print("hey! t`es née au printemps ")
+
+if hiver:
+    print("ta tu fret? t`es née en hiver ")
+
+if printemps:
+    print("c`est lmenage du pritemps, t`es née pour sa")
