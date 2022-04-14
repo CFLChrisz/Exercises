54846b36f1e32bbf60205468c2b86f6ca9a286d8
CFLChrisz <100291263+CFLChrisz@users.noreply.github.com>
2022-04-14 10:31:28 -0400

    Update login.py

--- semaine 7/login.py
+++ semaine 7/login.py
@@ -5,22 +5,22 @@ for l in lines:
     bd.append(l.split())
 f.close()
 print(bd)
-user1=bd[0][0]
-user2=bd[1][0]
-user3=bd[2][0]
-admin=bd[3][0]
-pass1=bd[0][1]
-pass2=bd[1][1]
-pass3=bd[2][1]
-adminpass=bd[3][1]    
+user1=bd[0]
+user2=bd[1]
+user3=bd[2]
+admin=bd[3]
+pass1=user1[1]
+pass2=user2[1]
+pass3=user3[1]
+adminpass=admin[1]    
 loggin = False
 while loggin==False:
     user = input('Hello! please enter your username: ')
     psswrd=input('Enter your password.: ')
-    if user==user1 and psswrd==pass1 or user==user2 and psswrd==pass2 or user==user3 and psswrd==pass3:
+    if user==user1[0] and psswrd==pass1 or user==user2[0] and psswrd==pass2 or user==user3[0] and psswrd==pass3:
             print('Welcome',user,)
             loggin=True
-    elif user==admin and psswrd==adminpass:
+    elif user==admin[0] and psswrd==adminpass:
         print('welcome Administaror',user,'!!!')
         loggin=True
         adminacc=True
@@ -33,7 +33,9 @@ while loggin==False:
             print('Thank you, goodbye!')
             quit()
 
-if loggin==True:
-    print(user)
-elif adminacc==True:
-    print(user)
+while loggin==True:
+    checking=user
+    print("How can we serve you today!\nType[1] for your checking account\nType[2] for your saving accout\nType[3] for your investment account\ntype[4] to logout\n")
+    option =int(input(''))
+    if option==1:
+        print(checking)
